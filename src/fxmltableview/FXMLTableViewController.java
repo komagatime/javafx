8352e294f86ff8b7e6ba60de05f2594f7ab348f1
komagatime <komagatime@gmail.com>
2012-12-01 14:22:15 +0900

    complete

--- src/fxmltableview/FXMLTableViewController.java
+++ src/fxmltableview/FXMLTableViewController.java
@@ -6,28 +6,34 @@ package fxmltableview;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  *
  * @author koma
  */
-public class FXMLTableViewController implements Initializable {
+public class FXMLTableViewController {
+    @FXML private TableView<Person> tableView;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField emailField;
     
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    protected void addPerson(ActionEvent event) {
+        ObservableList<Person> data = tableView.getItems();
+        data.add(new Person(firstNameField.getText(),
+            lastNameField.getText(),
+            emailField.getText()
+        ));
+        
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");   
     }
-    
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
 }
